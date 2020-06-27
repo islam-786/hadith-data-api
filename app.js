ff021ecf0af11d22d890c7c77fca2eb0d75ff01c
AxeemHaider <axeemhaider@gmail.com>
2020-06-27 10:30:40 +0500

    complete

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+//const morgan = require("morgan");
+
+const { Firestore } = require("@google-cloud/firestore");
+const firestore = new Firestore();
+
+const app = express();
+
+app.set("case sensitive routing", true);
+app.use(bodyParser.json());
+
+//app.use(morgan("dev"));
+
+app.get("/:id", async (req, res) => {
+  const hadithSnap = await firestore
+    .collection("hadith_bukhari")
+    .doc(req.params.id)
+    .get();
+
+  if (hadithSnap.exists) {
+    res.status(200).json({
+      id: req.params.id,
+      result: hadithSnap.data(),
+    });
+  } else {
+    res.status(200).json({
+      message: "Hadith not found",
+    });
+  }
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log("Press Ctrl+C to quit.");
+});
+// [END gae_node_request_example]
+
+module.exports = app;
