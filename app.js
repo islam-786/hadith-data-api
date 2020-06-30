62d2f5c1db8b46a64d30e6a6cf371be492403479
AxeemHaider <axeemhaider@gmail.com>
2020-06-30 15:42:03 +0500

    allow cros

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const firestore = new Firestore();
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.set("case sensitive routing", true);
 app.use(bodyParser.json());
 
